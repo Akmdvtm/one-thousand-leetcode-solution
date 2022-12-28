4b4aa0bec234c03a3d5df927bb2543d53848c43c
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2022-12-28 20:57:45 +0600

    Palindrome Number

---
+++ Palindrome Number/isPolindrome.go/ispalindrome.go
@@ -0,0 +1,23 @@
+package ispalindrome
+
+import "strconv"
+
+func isPalindrome(x int) bool {
+	runes := []rune(strconv.Itoa(x))
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+
+	return string(runes) == strconv.Itoa(x)
+}
+
+//v2
+
+// func isPalindrome(x int) bool {
+// 	runes := []rune(strconv.Itoa(x))
+// 	res := []rune{}
+// 	for i := len(runes) - 1; i >= 0; i-- {
+// 		res = append(res, runes[i])
+// 	}
+// 	return string(res) == strconv.Itoa(x)
+// }
