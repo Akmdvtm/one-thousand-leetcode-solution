24ac993868a41ac1a065b12931c9822493192dd9
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-28 15:01:50 +0600

    Single Number

---
+++ Single Number/sn.js
@@ -0,0 +1,13 @@
+var singleNumber = function (nums) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hash[nums[i]] ? (hash[nums[i]] += 1) : (hash[nums[i]] = 1);
+  }
+
+  for (let key in hash) {
+    if (hash[key] === 1) {
+      return key;
+    }
+  }
+};
